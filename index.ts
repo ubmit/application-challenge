1e52cf258e91771ec1882559572cf527baf28ec0
Guilherme de Andrade <guilhermedeandrade@protonmail.com>
2024-02-08 16:05:11 +0000

    refactor: create a fetcher with the necessary headers built-in

--- index.ts
+++ index.ts
@@ -1,30 +1,22 @@
-async function firstCall() {
-  const response = await fetch(
-    "https://letsrevolutionizetesting.com/challenge",
-    {
-      headers: { accept: "application/json" },
-    }
-  );
+async function fetchJson(url: string) {
+  const response = await fetch(url, {
+    headers: { accept: "application/json" },
+  });
   return response.json();
 }
 
 async function chase(url: string) {
   if (!url) return;
-  const response = await fetch(url, {
-    headers: { accept: "application/json" },
-  });
-  const json = await response.json();
-  console.log(json);
-  chase(json.follow);
+  const { follow, message } = await fetchJson(url);
+  console.log(message);
+  chase(follow);
 }
 
 async function main() {
-  const { follow } = await firstCall();
-  const response = await fetch(follow, {
-    headers: { accept: "application/json" },
-  });
-  const json = await response.json();
-  await chase(json.follow);
+  const { follow } = await fetchJson(
+    "https://letsrevolutionizetesting.com/challenge"
+  );
+  chase(follow);
 }
 
 main();
