2ac3430c15b5789299f842061c3a9927cda974ba
Guilherme de Andrade <guilhermedeandrade@protonmail.com>
2024-02-08 15:55:21 +0000

    feat: add a recursive fn to keep requesting until there is no url to follow

--- index.ts
+++ index.ts
@@ -1,1 +1,30 @@
+async function firstCall() {
+  const response = await fetch(
+    "https://letsrevolutionizetesting.com/challenge",
+    {
+      headers: { accept: "application/json" },
+    }
+  );
+  return response.json();
+}
 
+async function chase(url: string) {
+  if (!url) return;
+  const response = await fetch(url, {
+    headers: { accept: "application/json" },
+  });
+  const json = await response.json();
+  console.log(json);
+  chase(json.follow);
+}
+
+async function main() {
+  const { follow } = await firstCall();
+  const response = await fetch(follow, {
+    headers: { accept: "application/json" },
+  });
+  const json = await response.json();
+  await chase(json.follow);
+}
+
+main();
